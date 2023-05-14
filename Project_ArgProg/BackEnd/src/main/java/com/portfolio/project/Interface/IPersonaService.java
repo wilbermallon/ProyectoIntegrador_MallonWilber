119bd39e2cc790495bcf160ddd0b3667e1402870
wilbermallon <wilbermallon@gmail.com>
2023-05-14 10:38:54 -0300

    creación de servicio para el BackEnd

---
+++ Project_ArgProg/BackEnd/src/main/java/com/portfolio/project/Interface/IPersonaService.java
@@ -0,0 +1,20 @@
+package com.portfolio.project.Interface;
+
+import com.portfolio.project.Entity.Persona;
+import java.util.List;
+
+
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> getPersona();
+    
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+}
